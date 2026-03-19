ba39a1cceb5932cb0e60696a799bcc629fc2fa2c
Vladislav Gutnik <lt38igreen@gmail.com>
2026-03-19 14:06:15 +0300

    Rewrite "listUsersByCar" method in UserDaoImpl with LEFT JOIN FETCH hql

--- src/main/java/hiber/dao/UserDaoImpl.java
+++ src/main/java/hiber/dao/UserDaoImpl.java
@@ -31,7 +31,8 @@ public class UserDaoImpl implements UserDao {
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsersByCar(String model, int series) {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("FROM User u WHERE u.car.model = :model AND u.car.series = :series");
+        String hql = "SELECT u FROM User u LEFT JOIN FETCH u.car c WHERE c.model=:model AND c.series =:series";
+                TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql);
         query.setParameter("model", model);
         query.setParameter("series", series);
         return query.getResultList();
